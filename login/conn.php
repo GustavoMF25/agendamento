a3bb1243eaa4243dd01628cb39543b49476b30db
Gustavo Fernandes -  TI Desenvolvimento de Sistemas <gustavo.fernandes@CORP>
2023-12-05 15:46:17 -0300

    Adicionando dotenv na aplicação

--- login/conn.php
+++ login/conn.php
@@ -1,16 +1,22 @@
 <?php
 
-$schemaBase = "agendamento";
-$con = mysqlI_connect("localhost", "root", "", $schemaBase);
+try {
+    $schemaBase = "syntaxweb_agendamento";
+    $con = mysqlI_connect("localhost", "root", "", $schemaBase);
 
-$sql = "SET NAMES 'utf8'";
-mysqli_query($con, $sql);
 
-$sql = 'SET character_set_connection=utf8';
-mysqli_query($con, $sql);
 
-$sql = 'SET character_set_client=utf8';
-mysqli_query($con, $sql);
+    $sql = "SET NAMES 'utf8'";
+    mysqli_query($con, $sql);
 
-$sql = 'SET character_set_results=utf8';
-$res = mysqli_query($con, $sql);
+    $sql = 'SET character_set_connection=utf8';
+    mysqli_query($con, $sql);
+
+    $sql = 'SET character_set_client=utf8';
+    mysqli_query($con, $sql);
+
+    $sql = 'SET character_set_results=utf8';
+    $res = mysqli_query($con, $sql);
+} catch (Exception $e) {
+    header("Location: ./start.php");
+}
